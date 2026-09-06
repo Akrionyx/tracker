3a7ff00034c5199223b795895cfcb1ba662b15bf
Akrionyx <parmarrkunal@gmail.com>
2026-09-06 15:48:26 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* Offline cache for the expense tracker. Bump CACHE when you upload a new build. */
-const CACHE = "tracker-v7";
+const CACHE = "finix-v10";
 const CORE = ["./", "./index.html", "./manifest.json"];
-const ICONS = ["./apple-touch-icon.png", "./icon-192.png", "./icon-512.png"];
+const ICONS = ["./apple-touch-icon.png", "./icon-192.png", "./icon-512.png", "./logo.png"];
 const NAV_TIMEOUT = 3000;
 
 self.addEventListener("install", (e) => {
